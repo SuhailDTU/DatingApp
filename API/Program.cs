34b865c4334cded70959ce749ee473cd2b55038a
Suhail Abdi <s205135@dtu.dk>
2023-05-08 16:52:56 +0200

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,11 +12,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(corsbuilder => corsbuilder.AllowAnyHeader().AllowAnyHeader().WithOrigins(new string [] {"http://localhost:4200", "https://localhost:4200"}));
+
 app.MapControllers();
 
 app.Run();
