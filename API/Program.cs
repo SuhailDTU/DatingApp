3b5be0c2c2f8f70ebfe0dfbe54e1d2f3a6aa07c3
Suhail Abdi <s205135@dtu.dk>
2023-05-26 15:22:41 +0200

    end of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,28 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(corsbuilder => corsbuilder.AllowAnyHeader().AllowAnyHeader().WithOrigins(new string [] {"http://localhost:4200", "https://localhost:4200"}));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
